d984a34a79aa71e379c1673cb183b2ff5f9c830d
Kirill <46571896+Kimoko@users.noreply.github.com>
2022-03-21 23:15:32 +0500

    //

--- src/pages/analysis.js
+++ src/pages/analysis.js
@@ -13,7 +13,7 @@ export default function Main() {
   <div>
      <div>
            <h1 className="text-center mt-3 mb-2">Disease Symptom Predictor</h1>
-           <h2 className="text-center mt-1 mb-2">Вам будут заданы вопросы, на которые необходимо ответить "Да" или "Нет"</h2>
+           <h4 className="text-center mt-1 mb-2">Вам будут заданы вопросы, на которые необходимо ответить "Да" или "Нет"</h4>
       </div>
         
        <div className="border border-primary rounded w-25 p-3 mx-auto mt-5">
@@ -31,7 +31,7 @@ export default function Main() {
         </div>
       </div>
       <div className='text-center '>
-      <Button className="button mt-3 btn-lg" href={ROUTES.MAIN}>Отправить</Button>
+      <Button className="button mt-3 btn-lg" href={ROUTES.RESULTS}>Отправить</Button>
       </div>
     </div>
   );
